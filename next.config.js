85b896142ed6d904ffff2f3293a9f2a43d5ade05
wellington01 <wellington.c.v.santana@gmail.com>
2025-05-12 14:40:53 -0300

    improve security headers and update packages version

--- next.config.js
+++ next.config.js
@@ -15,6 +15,40 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    // Headers de segurança
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY",
+                    },
+                    {
+                        key: "Content-Security-Policy",
+                        value: "frame-ancestors 'none'",
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff",
+                    },
+                    {
+                        key: "Referrer-Policy",
+                        value: "strict-origin-when-cross-origin",
+                    },
+                    {
+                        key: "Permissions-Policy",
+                        value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+                    },
+                    {
+                        key: "Strict-Transport-Security",
+                        value: "max-age=31536000; includeSubDomains",
+                    },
+                ],
+            },
+        ];
+    },
     images: {
         remotePatterns: [
             {
