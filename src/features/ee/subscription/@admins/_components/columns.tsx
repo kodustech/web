fa0cc1fda7141fb5b57c1bb59398cdf0904daa1c
Erick Riva <erickriva@hotmail.com.br>
2025-08-18 17:14:32 -0300

    [feat] added `copy invite link` button to `organization admins` tab

--- src/features/ee/subscription/@admins/_components/columns.tsx
+++ src/features/ee/subscription/@admins/_components/columns.tsx
@@ -5,12 +5,15 @@ import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
+    DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@components/ui/dropdown-menu";
 import { magicModal } from "@components/ui/magic-modal";
+import { toast } from "@components/ui/toaster/use-toast";
 import type { MembersSetup } from "@services/setup/types";
 import { ColumnDef } from "@tanstack/react-table";
 import { EllipsisVertical } from "lucide-react";
+import { ClipboardHelpers } from "src/core/utils/clipboard";
 
 import { DeleteModal } from "./delete-modal";
 
@@ -37,6 +40,30 @@ export const columns: ColumnDef<MembersSetup>[] = [
                     </DropdownMenuTrigger>
 
                     <DropdownMenuContent align="end">
+                        <DropdownMenuItem
+                            onClick={async () => {
+                                await ClipboardHelpers.copyTextToClipboard(
+                                    `${window.location.origin}/invite/${row.original.userId}`,
+                                );
+
+                                toast({
+                                    variant: "info",
+                                    title: "Copied to clipboard the invite link",
+                                    description: (
+                                        <span className="text-text-secondary">
+                                            for user with email{" "}
+                                            <span className="text-text-primary">
+                                                {row.original.email}
+                                            </span>
+                                        </span>
+                                    ),
+                                });
+                            }}>
+                            Copy invite link
+                        </DropdownMenuItem>
+
+                        <DropdownMenuSeparator />
+
                         <DropdownMenuItem
                             className="text-danger"
                             onClick={async () => {
