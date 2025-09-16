dcdd87b5b368604b2e70d10b38c946893a79738c
Erick Riva <erickriva@hotmail.com.br>
2025-09-16 18:01:54 -0300

    [fix] trying to go to `/` route when setup is unfinished, redirects to cockpit with errors because there's no active team

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,12 +1,14 @@
 import { redirect } from "next/navigation";
-import { SetupAndOnboardingLock } from "@components/system/setup-lock";
 import {
     getOrganizationId,
     getOrganizationName,
 } from "@services/organizations/fetch";
+import { getTeamParameters } from "@services/parameters/fetch";
+import { ParametersConfigKey } from "@services/parameters/types";
 import { getTeams } from "@services/teams/fetch";
 import { auth } from "src/core/config/auth";
 import { NavMenu } from "src/core/layout/navbar";
+import { TEAM_STATUS } from "src/core/types";
 import { getGlobalSelectedTeamId } from "src/core/utils/get-global-selected-team-id";
 import { getFeatureFlagWithPayload } from "src/core/utils/posthog-server-side";
 import { FinishedTrialModal } from "src/features/ee/subscription/_components/finished-trial-modal";
@@ -23,22 +25,34 @@ export default async function Layout({ children }: React.PropsWithChildren) {
     const session = await auth();
     if (!session) redirect("/sign-out");
 
-    const [teams, teamId, organizationId, organizationName] = await Promise.all(
-        [
-            getTeams(),
-            getGlobalSelectedTeamId(),
-            getOrganizationId(),
-            getOrganizationName(),
-        ],
-    );
+    const teams = await getTeams();
+
+    if (!teams.some((team) => team.status === TEAM_STATUS.ACTIVE)) {
+        redirect("/setup");
+    }
+
+    const teamId = await getGlobalSelectedTeamId();
+
+    const platformConfigs = await getTeamParameters<{
+        configValue: { finishOnboard?: boolean };
+    }>({
+        key: ParametersConfigKey.PLATFORM_CONFIGS,
+        teamId,
+    });
+
+    if (!platformConfigs?.configValue?.finishOnboard) redirect("/setup");
 
     const [
+        organizationId,
+        organizationName,
         organizationLicense,
         usersWithAssignedLicense,
         issuesPageFeatureFlag,
         logsPagesFeatureFlag,
         pullRequestsPageFeatureFlag,
     ] = await Promise.all([
+        getOrganizationId(),
+        getOrganizationName(),
         validateOrganizationLicense({ teamId }),
         getUsersWithLicense({ teamId }),
         getFeatureFlagWithPayload({ feature: "issues-page" }),
@@ -50,14 +64,10 @@ export default async function Layout({ children }: React.PropsWithChildren) {
         <Providers
             session={session}
             teams={teams}
-            organization={{
-                id: organizationId,
-                name: organizationName,
-            }}>
+            organization={{ id: organizationId, name: organizationName }}>
             <SubscriptionProvider
                 license={organizationLicense}
                 usersWithAssignedLicense={usersWithAssignedLicense}>
-                <SetupAndOnboardingLock />
                 <NavMenu
                     issuesPageFeatureFlag={issuesPageFeatureFlag}
                     logsPagesFeatureFlag={logsPagesFeatureFlag}
