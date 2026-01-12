8cc7a505e794efb8b86feb146b3596622458cb8c
Gabriel Malinosqui <gabriel@kodus.io>
2026-01-12 10:35:43 -0300

    Add CLI keys option to organization sidebar
    - Introduced KeyRoundIcon for the new CLI keys section.
    - Added link to CLI keys in the sidebar, making it always visible.

--- src/app/(app)/organization/layout.tsx
+++ src/app/(app)/organization/layout.tsx
@@ -6,18 +6,26 @@ import { getFeatureFlagWithPayload } from "src/core/utils/posthog-server-side";
 import { ConfigsSidebar } from "./_components/sidebar";
 
 export default async function Layout(props: PropsWithChildren) {
-    const ssoFeatureFlag = await getFeatureFlagWithPayload({
-        feature: FEATURE_FLAGS.sso,
-    });
+    const [ssoFeatureFlag, cliKeysFeatureFlag] = await Promise.all([
+        getFeatureFlagWithPayload({ feature: FEATURE_FLAGS.sso }),
+        getFeatureFlagWithPayload({ feature: FEATURE_FLAGS.cliKeys }),
+    ]);
+
+    const ssoEnabled =
+        typeof ssoFeatureFlag?.value === "boolean"
+            ? ssoFeatureFlag.value
+            : false;
+    const cliKeysEnabled =
+        typeof cliKeysFeatureFlag?.value === "boolean"
+            ? cliKeysFeatureFlag.value
+            : false;
 
     return (
         <div className="flex flex-1 flex-row overflow-hidden">
             <ConfigsSidebar
                 featureFlags={{
-                    sso:
-                        (typeof ssoFeatureFlag?.value === "boolean"
-                            ? ssoFeatureFlag.value
-                            : false) ?? false,
+                    [FEATURE_FLAGS.sso]: ssoEnabled,
+                    [FEATURE_FLAGS.cliKeys]: cliKeysEnabled,
                 }}
             />
             <Page.WithSidebar>{props.children}</Page.WithSidebar>
